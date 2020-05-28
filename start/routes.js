cb87a806e0f2d3c12bf65e73ab0db0624f34d1ca
matheus <matheuscleber1998@gmail.com>
2020-05-28 18:21:40 -0300

    Uploload Files

--- start/routes.js
+++ start/routes.js
@@ -19,4 +19,6 @@ const Route = use('Route')
 Route.post('users', 'UserController.store')
 Route.post('sessions', 'SessionController.store')
 Route.post('passwords', 'ForgotPasswordController.store')
+Route.post('files', 'FileController.store')
+
 Route.put('passwords', 'ForgotPasswordController.update')
