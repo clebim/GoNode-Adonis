2305d8c3f9d563a4571e774e6f82d86cbee4bb5b
matheus <matheuscleber1998@gmail.com>
2020-06-01 17:16:00 -0300

    feature: Migration and CRUD, Project, task

--- start/routes.js
+++ start/routes.js
@@ -16,11 +16,17 @@
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
-Route.get('/files/:id', 'FileController.show')
 
 Route.post('/users', 'UserController.store')
 Route.post('/sessions', 'SessionController.store')
 Route.post('/passwords', 'ForgotPasswordController.store')
-Route.post('/files', 'FileController.store')
+Route.put('/passwords', 'ForgotPasswordController.update')
 
-Route.put('passwords', 'ForgotPasswordController.update')
+Route.get('/files/:id', 'FileController.show')
+
+Route.group( () => {
+  Route.resource('/projects', 'ProjectController').apiOnly()
+  Route.resource('projects.tasks', 'TaskController').apiOnly()
+
+  Route.post('/files', 'FileController.store')
+}).middleware(['auth'])
