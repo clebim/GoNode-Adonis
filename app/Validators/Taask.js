7911b7ca622dc7d2f8ff2d8ce206634f55199cba
matheus <matheuscleber1998@gmail.com>
2020-06-01 19:57:02 -0300

    feature: Route validation

---
+++ app/Validators/Taask.js
@@ -0,0 +1,17 @@
+'use strict'
+
+class Taask {
+  
+  get validateAll () {
+    return true
+  }
+
+  get rules () {
+    return {
+      title: 'required',
+      due_date: 'date'
+    }
+  }
+}
+
+module.exports = Taask
